df9171cb2821d3705244a40f3abbcbc803de8c1a
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2015-07-25 16:40:41 -0600

    Fix issue when is in production mode

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,18 +16,20 @@ angular
     'ngSanitize',
     'ngTouch'
   ])
-  .config(function($routeProvider) {
-    $routeProvider
-      .when('/post', {
-        templateUrl: 'views/post.html',
-        controller: 'PostCtrl'
-      })
-      .when('/:page?', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        reloadOnSearch: false
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+  .config([
+    '$routeProvider',
+    function($routeProvider) {
+      $routeProvider
+        .when('/post', {
+          templateUrl: 'views/post.html',
+          controller: 'PostCtrl'
+        })
+        .when('/:page?', {
+          templateUrl: 'views/main.html',
+          controller: 'MainCtrl',
+          reloadOnSearch: false
+        })
+        .otherwise({
+          redirectTo: '/'
+        });
+    }]);

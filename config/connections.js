815b6c2c18411eea8e5001cec36921b0d6244b6a
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2015-07-25 17:55:21 -0600

    Install and configure MongoDD store

--- config/connections.js
+++ config/connections.js
@@ -56,13 +56,10 @@ module.exports.connections = {
   * Run: npm install sails-mongo                                             *
   *                                                                          *
   ***************************************************************************/
-  someMongodbServer: {
+  mongoLabHeroku: {
     adapter: 'sails-mongo',
-    host: 'localhost',
-    port: 27017,
-    // user: 'username',
-    // password: 'password',
-    // database: 'your_mongo_db_name_here'
+    url: process.env.MONGOLAB_URI || 'mongodb://localhost:27017/voteApp',
+    schema: true
   },
 
   /***************************************************************************
--- config/env/production.js
+++ config/env/production.js
@@ -17,15 +17,15 @@ module.exports = {
    * environment (see config/connections.js and config/models.js )           *
    ***************************************************************************/
 
-  // models: {
-  //   connection: 'someMysqlServer'
-  // },
+  models: {
+    connection: 'mongoLabHeroku'
+  },
 
   /***************************************************************************
    * Set the port in the production environment to 80                        *
    ***************************************************************************/
 
-  // port: 80,
+  port: process.env.PORT || 1337,
 
   /***************************************************************************
    * Set the log level in production environment to "silent"                 *
